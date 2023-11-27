50c405ede12cde2ddb9bd0e8c314a9b3f6af24d1
Federico Ocampo <30559203+Federico19@users.noreply.github.com>
2023-11-27 07:54:39 -0300

    Update Circuito.py

--- Circuito.py
+++ Circuito.py
@@ -1,26 +1,23 @@
 from machine import Pin
-from time import gmtime
+import utime
 
 class Circuito():
   def __init__(self, maquina):
-    self.sensor_prox = Pin(15, Pin.IN)
-    self.led_prox = Pin(19, Pin.OUT)
+    self.sensor_prox = Pin(15, Pin.IN, value = 1)
+    self.led_prox = Pin("LED", Pin.OUT)
     self.maquina = maquina
-    self.control_tiempo = -1
   
   def control_sensor_prox(self):
     if self.sensor_prox.value() == 0:
         self.led_prox.on()
-        control_reloj = gmtime()[5]
-        if control_reloj > 29:
-            self.control_tiempo = control_reloj - 30
-        else:
-            self.control_tiempo = control_reloj + 30
         self.maquina.prender_maquina()
-
-    control_reloj = gmtime()[5]
-    if self.control_tiempo  == control_reloj:
-        self.control_tiempo = -1
-        self.maquina.apagar_maquina()
+        self.maquina.clock_inicial = utime.ticks_ms()
+    else:
+        self.led_prox.off()
     
-    self.maquina.control_encoder()
+    lapso_tiempo = utime.ticks_diff(utime.ticks_ms(), self.maquina.clock_inicial)
+
+    if self.maquina.encendida:
+      if lapso_tiempo > 30000:
+         self.maquina.apagar_maquina()
+      self.maquina.control_encoder()
